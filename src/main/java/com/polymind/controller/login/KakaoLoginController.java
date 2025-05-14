972c7b0fbd84fb7f40cad4b619cb4645378205cd
raon98 <cheol1998@kakao.com>
2025-05-14 16:22:13 +0900

    kakao login

--- src/main/java/com/polymind/controller/login/KakaoLoginController.java
+++ src/main/java/com/polymind/controller/login/KakaoLoginController.java
@@ -1,6 +1,9 @@
 package com.polymind.controller.login;
+
+import com.polymind.dto.req.login.KakaoRequest;
 import com.polymind.support.utils.ApiResponseEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/api/kakao")
 public class KakaoLoginController {
 
-    @RequestMapping("/login")
-    public ApiResponseEntity<?> login(@RequestBody String code){
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponseEntity<?>> login(@RequestBody KakaoRequest request){
 
-        return ApiResponseEntity.builder()
-                .data("test")
-                .build();
+        String code = request.getCode();
+        return ResponseEntity.ok(
+                ApiResponseEntity.builder()
+                        .data(code)
+                        .build()
+        );
     }
 }
---
+++ src/main/java/com/polymind/dto/req/login/KakaoRequest.java
@@ -0,0 +1,8 @@
+package com.polymind.dto.req.login;
+
+import lombok.Data;
+
+@Data
+public class KakaoRequest {
+    private String code;
+}
