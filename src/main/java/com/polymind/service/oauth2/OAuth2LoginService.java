8d04ff705d8609c34da827213edd30fe1ed27ce9
raon98 <cheol1998@kakao.com>
2025-05-19 17:42:13 +0900

    FIX : kakao 맵핑 수정

--- src/main/java/com/polymind/service/oauth2/OAuth2LoginService.java
+++ src/main/java/com/polymind/service/oauth2/OAuth2LoginService.java
@@ -3,6 +3,7 @@ package com.polymind.service.oauth2;
 import com.polymind.dto.request.oauth2.OAuth2LoginRequest;
 import com.polymind.dto.response.oauth.OAuth2AccessTokenResponse;
 import com.polymind.dto.response.oauth.OAuth2LoginResult;
+import com.polymind.dto.response.oauth.kakao.KakaoUser;
 import com.polymind.entity.user.User;
 
 public interface OAuth2LoginService {
